8608d1a9f580b6c252cbe52462ea9ff90af71929
sudaiskhanDev <sudaisliveofficial@gmail.com>
2025-11-03 21:31:28 +0500

    login

--- src/app/ArticleGen/page.jsx
+++ src/app/ArticleGen/page.jsx
@@ -3,18 +3,18 @@ import React from 'react'
 const page = () => {
   return ( 
     <>
-    <div className='bg-black h-screen flex justify-center items-center'>
-        <div className="main-input-output w-[90%] md:w-[80%] max-w-4xl h-[800px] mx-auto mt-12 mb-8 rounded-2xl shadow-lg overflow-hidden flex flex-col border bg-gray-500 border-gray-900">
+    <div className='bg-[#000000] h-screen flex justify-center items-center'>
+        <div className="main-input-output w-[90%] md:w-[80%] max-w-4xl h-[800px] mx-auto mt-12 mb-8 rounded-2xl shadow-lg overflow-hidden flex flex-col border bg-[#212121] border-gray-900">
         {/* Output Section */}
-        <div className="output-section flex-grow p-6 overflow-y-auto bg-gray-700">
+        <div className="output-section flex-grow p-6 overflow-y-auto">
             <p className="text-white leading-relaxed">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt temporibus sed officiis tempora cumque aliquid sit, amet quisquam, dolor provident quas non vero repellat, corrupti voluptatem aut. Molestias, voluptas recusandae?
             </p>
         </div>
 
         {/* Input Section */}
-        <div className="input-section w-full h-[150px] bg-gray-700 flex justify-center items-center p-4">
-            <div className="input-text-btn w-full sm:w-[80%] md:w-[60%] h-[60px] bg-[#8a8a8a] rounded-full shadow-md flex items-center px-2">
+        <div className="input-section w-full h-[150px]  flex justify-center items-center p-4">
+            <div className="input-text-btn w-full sm:w-[80%] md:w-[60%] h-[60px]  rounded-full shadow-md flex items-center px-2 bg-gray-500 ">
                 <div className="text-input flex-grow h-[40px] overflow-hidden">
                     <input 
                         type="text"
