df1f4bd9976578a6a9755d712d93fa7c7edb0246
sudaiskhanDev <sudaisliveofficial@gmail.com>
2025-11-05 11:36:44 +0500

    logo

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@ import Header from '@/components/Main/Header'
 import TeamsLogo from '@/components/Main/TeamsLogo'
 import ToolsSection from '@/components/Main/ToolsSection'
 import NavBar from '@/components/NavBar'
+import { Marquee } from '@/components/ui/marquee'
 import React from 'react'
 
 const page = () => {
@@ -11,7 +12,12 @@ const page = () => {
    <Header />
    <TeamsLogo />
    <ToolsSection />
- 
+<Marquee>
+  <span>Next.js</span>
+  <span>React</span>
+  <span>TypeScript</span>
+  <span>Tailwind CSS</span>
+</Marquee> 
    </>
   )
 }
